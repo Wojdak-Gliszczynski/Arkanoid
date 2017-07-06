c89fdf1d4576d76443c3f149af09f89efc4a045f
RadoslawWojdak <need4git@gmail.com>
2017-07-06 20:43:57 +0200

    System odbijający piłeczki
    -Dodano metodę "Bounce" do klasy "Ball" odpowiedzialną za odbijanie się
    piłeczki od prtosokątów;
    -Poprawiono setter atrybutu "_angle" klasy "Ball". Teraz konwertuje
    liczby mniejsze od 0 do przedziału <0; 2*PI>.

--- Arkanoid/Arkanoid/Ball.cs
+++ Arkanoid/Arkanoid/Ball.cs
@@ -10,6 +10,7 @@ namespace Arkanoid
 {
     class Ball : TransformingImage
     {
+        private Rect lastCollisionArea; //Obszar kolizji z poprzedniego ruchu
         private double _speed;
         private double _angle;   //W radianach
 
@@ -25,7 +26,14 @@ namespace Arkanoid
         public double Angle
         {
             get { return _angle; }
-            set { _angle = value % (2 * Math.PI); }
+            set
+            {
+                while (value < 0)
+                {
+                    value += 2 * Math.PI;
+                }
+                _angle = value % (2 * Math.PI);
+            }
         }
         //-------------------------------------------------------
         public Ball(Grid grid) : base(new Uri("./Graphics/ball-0.png", UriKind.Relative), grid, 384, 456)
@@ -36,10 +44,23 @@ namespace Arkanoid
         //-------------------------------------------------------
         public void Move()
         {
+            lastCollisionArea = new Rect(Margin.Left, Margin.Top, Width, Height);
+
             double speedX = Math.Cos(Angle) * Speed;
             double speedY = Math.Sin(Angle) * Speed;
 
             this.Move(speedX, speedY);
+        }
+        //-------------------------------------------------------
+        public void Bounce(Rect rect)
+        {
+            //Z której części prostokąta się odbija (przejście między ćwiartkami)
+            if ((lastCollisionArea.Top >= rect.Bottom) || (lastCollisionArea.Bottom <= rect.Top))    //Odbija się od dolnej lub górnej części prostokąta
+                Angle = Math.PI - (Angle - Math.PI);
+            else if ((lastCollisionArea.Right <= rect.Left) || (lastCollisionArea.Left >= rect.Right))   //Odbija się od lewej lub prawej części prostokąta
+                Angle = Math.PI * 0.5 - (Angle - Math.PI * 0.5);
+
+            Move(); //Wymagany, żeby piłka nie znajdywała się cały czas wewnątrz prostokąta
         }
     }
 }
